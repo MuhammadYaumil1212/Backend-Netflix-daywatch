143d502c4e8f515a23cb7a959faf902b0dc4d30f
MuhammadYaumil1212 <54166418+MuhammadYaumil1212@users.noreply.github.com>
2025-02-11 18:25:00 +0700

    Update envVars.js

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
+	MONGO_URI:"mongodb+srv://yaumilramkhadani:4cwvYJaUVPnd2J4N@movie-api.qam09.mongodb.net/?retryWrites=true&w=majority&appName=movie-api",
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV,
